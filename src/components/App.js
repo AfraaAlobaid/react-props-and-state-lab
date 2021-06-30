e521f57229de02c0e340ca515bb7aa15059ba86f
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-06-30 15:33:09 +0930

    implement pet adoption site

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,93 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
+        type: "all",
+      },
+    };
+  }
+
+  changeFilterType = (type) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type,
+      },
+    });
+  };
+
+  findPets = () => {
+    console.log("I am here!")
+    switch (this.state.filters.type) {
+      case "all":
+        fetch("/api/pets")
+          .then((response) => response.json())
+          .then((data) =>
+            this.setState({
+              pets: data,
+            })
+          );
+        break;
+      case "cat":
+        fetch("/api/pets?type=cat")
+          .then((response) => response.json())
+          .then((data) =>
+            this.setState({
+              pets: data,
+            })
+          );
+        break;
+      case "dog":
+        fetch("/api/pets?type=dog")
+          .then((response) => response.json())
+          .then((data) =>
+            this.setState({
+              pets: data,
+            })
+          );
+        break;
+      case "micropig":
+        fetch("/api/pets?type=micropig")
+          .then((response) => response.json())
+          .then((data) =>
+            this.setState({
+              pets: data,
+            })
+          );
+        break;
+      default:
+        fetch("/api/pets")
+          .then((response) => response.json())
+          .then((data) =>
+            this.setState({
+              pets: data,
+            })
+          );
     }
+
+    console.log(this.state.pets);
+  };
+
+  changeAdoptStatus = (id) => {
+    const index = this.state.pets.findIndex((e) => e.id === id);
+    this.state.pets[index].isAdopted = true;
+    // this.setState({
+    //   pets: [
+    //     ...this.state.pets,
+    //     [index]: {
+    //       ...this.state.pets[index],
+    //       isAdopted: true
+    //     }
+    //   ]
+    // })
   }
 
   render() {
@@ -24,16 +99,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.changeFilterType}
+                onFindPetsClick={this.findPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.changeAdoptStatus} pets={this.state.pets}/>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
